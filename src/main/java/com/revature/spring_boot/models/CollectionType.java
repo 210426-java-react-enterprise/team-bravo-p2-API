c4e0da1e15c290d0f62bd03793fe60ab478316de
jbialon32 <jbialon32@gmail.com>
2021-06-07 20:11:49 -0500

    working with models

---
+++ src/main/java/com/revature/spring_boot/models/CollectionType.java
@@ -0,0 +1,20 @@
+package com.revature.spring_boot.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jbialon
+ * Date: 6/7/2021
+ * Time: 6:46 PM
+ * Description: {Insert Description}
+ */
+
+@Entity
+@Table(name = "collection_types")
+public class CollectionType {
+
+    
+
+}
