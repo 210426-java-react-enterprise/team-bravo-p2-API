9d91e5cc571ecf3818f15093c0a1bb155ee3e42d
annaisha-sys <83236719+annaisha-sys@users.noreply.github.com>
2021-06-09 18:09:00 -0400

    clean up of update to now update by id and addition/clean up of delete HTTP method.

--- src/main/java/com/revature/spring_boot/services/MovieCollectionService.java
+++ src/main/java/com/revature/spring_boot/services/MovieCollectionService.java
@@ -6,6 +6,7 @@ import com.revature.spring_boot.exceptions.ResourceNotFoundException;
 import com.revature.spring_boot.models.MovieCollections;
 import com.revature.spring_boot.repos.CollectionInfoRepository;
 import com.revature.spring_boot.repos.MovieCollectionsRepository;
+import com.revature.spring_boot.web.dtos.MovieCollectionsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -54,20 +55,52 @@ public class MovieCollectionService {
 
     }
 
-    public ResponseEntity<String> updateMovieCollection(MovieCollections movieRequest) {
 
-        Optional<MovieCollections> movieCollections = collectionInfoRepo.findById(movieRequest.getMovies().getMovieId());
-        if(movieCollections.isPresent()) {
-            movieCollections.get().setUserRating(movieRequest.getUserRating());
-            movieCollections.get().setTradeable(movieRequest.getTradeable());
-            movieCollections.get().setUserComment(movieRequest.getUserComment());
-            movieCollections.get().setWatched(movieRequest.getWatched());
-            collectionInfoRepo.save(movieCollections.get());
-            return new ResponseEntity<String>("Your collection has been updated successfully", HttpStatus.OK);
-        }
 
-        return new ResponseEntity<String>("Sorry, we cannot find that collection item"+ movieRequest.getMovies().getMovieId(), HttpStatus.BAD_REQUEST);
+    public MovieCollections updateMovieCollectionById(int movieCollectionsId, MovieCollections movieCollect) {
+
+    if(collectionInfoRepo.findById(movieCollectionsId).isPresent()) {
+        MovieCollections existingCollectionItem = collectionInfoRepo.findById(movieCollectionsId).get();
+
+        existingCollectionItem.setUserComment(movieCollect.getUserComment());
+        existingCollectionItem.setTradeable(movieCollect.getTradeable());
+        existingCollectionItem.setWatched(movieCollect.getWatched());
+        existingCollectionItem.setUserRating(movieCollect.getUserRating());
+
+        MovieCollections updatedCollectionItem = collectionInfoRepo.save(existingCollectionItem);
+
+        return updatedCollectionItem;
+    }
+        return null;
+    }
+
+
+//return of Optional may be better, to avoid null pointer exceptions.
+//    public ResponseEntity<String> updateMovieCollections(int movieCollectionsId, MovieCollections movieRequest) {
+//
+//        Optional<MovieCollections> movieCollections = collectionInfoRepo.findById(movieRequest.getMovies().getMovieId());
+//        if(movieCollections.isPresent()) {
+//            movieCollections.get().setUserRating(movieRequest.getUserRating());
+//            movieCollections.get().setTradeable(movieRequest.getTradeable());
+//            movieCollections.get().setUserComment(movieRequest.getUserComment());
+//            movieCollections.get().setWatched(movieRequest.getWatched());
+//            collectionInfoRepo.save(movieCollections.get());
+//            return new ResponseEntity<String>("Your collection has been updated successfully", HttpStatus.OK);
+//        }
+//
+//        return new ResponseEntity<String>("Unfortunately, we cannot find that collection item"+ movieRequest.getMovies().getMovieId(), HttpStatus.BAD_REQUEST);
+//
+//    }
+
+
+    public void deleteCollectionById(int movieCollectionsId) {
+        MovieCollections existingItem = collectionInfoRepo.findById(movieCollectionsId)
+                .orElseThrow(() -> new ResourceNotFoundException());
+
+        collectionInfoRepo.delete(existingItem);
+    }
+
+
 
     }
 
-}
