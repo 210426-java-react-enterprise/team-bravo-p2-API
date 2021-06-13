7845de6a806162efc1eceb1837d037957ac4ac92
jbialon32 <jbialon32@gmail.com>
2021-06-13 16:22:02 -0500

    Added route to add new movies to databse if they dont already exist in db

--- src/main/java/com/revature/spring_boot/repos/MovieRepository.java
+++ src/main/java/com/revature/spring_boot/repos/MovieRepository.java
@@ -2,8 +2,24 @@ package com.revature.spring_boot.repos;
 
 import com.revature.spring_boot.models.Movies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movies, Integer> {
+
+    @Transactional
+    @Query(
+            value =
+                    "select * from movies where (title = :title and year = :year and mpaa_rating = :mpaaRating and length_min = :lengthMin and genre = :genre and descrip = :descrip and prod_company = :prodCompany)",
+            nativeQuery = true)
+    List<Movies> exists(@Param("title") String title, @Param("year") int year,
+                        @Param("mpaaRating") String mpaaRating, @Param("lengthMin") int lengthMin,
+                        @Param("genre") String genre, @Param("descrip") String descrip,
+                        @Param("prodCompany") String prodCompany);
+
 }
--- src/main/java/com/revature/spring_boot/web/controllers/MovieController.java
+++ src/main/java/com/revature/spring_boot/web/controllers/MovieController.java
@@ -1,14 +1,15 @@
 package com.revature.spring_boot.web.controllers;
 
+import com.revature.spring_boot.models.Movies;
 import com.revature.spring_boot.services.MovieService;
 import com.revature.spring_boot.web.dtos.MovieDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +33,16 @@ public class MovieController {
                 .map(MovieDTO::new)
                 .collect(Collectors.toList());
         return movieDTOList;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE, value = "/save")
+    @ResponseBody
+    public MovieDTO saveNewMovie(@RequestBody @Valid MovieDTO newMovie){
+
+        MovieDTO movieDTO = new MovieDTO(movieService.saveNewMovie(newMovie));
+
+        return movieDTO;
     }
 
 }
